5d8749b1d1d499f578b2687bfcc5ace7ef86e54e
TheWmbat <ambre.spehner@protonmail.com>
2025-04-22 20:26:14 -0400

    diffview colours

--- lua/wmbat/plugins/colorscheme.lua
+++ lua/wmbat/plugins/colorscheme.lua
@@ -15,6 +15,7 @@ return {
                 'telescope',
                 'treesitter',
                 'lsp',
+                'diffview',
             },
             languages = {
                 'cpp',
